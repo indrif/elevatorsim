ce4ab77c3b3dfd3f9093ec828b0d63d75622d6f8
Daniel Winther <daniel.winther@blocket.se>
2014-07-09 09:27:25 +0200

    Fix jshint errors in office scenario

--- scenario/office.js
+++ scenario/office.js
@@ -8,7 +8,7 @@ define(["underscore", "traveler", "elevator"], function(_, Traveler, Elevator) {
         var elevatorsBusy = _.filter(systemState.elevators, function(item) {
             return item.state !== "still" || item.pushedButtons.length > 0 || item.travelers.length > 0;
         });
-        return systemState.callingFloors.length == 0 && elevatorsBusy.length == 0;
+        return systemState.callingFloors.length === 0 && elevatorsBusy.length === 0;
     }
 
     function getRandomCount(currentTick, maxAtTime, devTime, maxCount) {
@@ -25,19 +25,19 @@ define(["underscore", "traveler", "elevator"], function(_, Traveler, Elevator) {
         }
     }
 
+    function getName() {
+        return chance.name();
+    }
+
     return function(options) {
         var seed = (options.randomseed) ? options.randomseed : +Date();
         var rng = seedrandom(seed, {global: true});
         var chance = new Chance(seed);
 
-        function getName() {
-            return chance.name();
-        }
-
         this.isFinished = function(currentTick, systemState) {
             // End if more than 50 ticks have passed or if the system state is empty after 3 ticks
             return currentTick > 30 && isEmptyState(systemState);
-        }
+        };
 
         this.getElevatorSetup = function(stats) {
             return [
@@ -45,7 +45,7 @@ define(["underscore", "traveler", "elevator"], function(_, Traveler, Elevator) {
                 new Elevator("HISS 2", 0, maxFloors, 2, 3, stats),
                 new Elevator("HISS 3", 0, maxFloors, 2, 3, stats)
             ];
-        }
+        };
 
         this.onTick = function(tick, system) {
             // 10 per hour
@@ -59,7 +59,7 @@ define(["underscore", "traveler", "elevator"], function(_, Traveler, Elevator) {
             if (tick > 15 && tick < 95) {
                 // Random spawn travelers during the day
                 var count = parseInt(Math.random() * 2);
-                for(var i = 0; i < count; i++) {
+                for(i = 0; i < count; i++) {
                     var from = parseInt(Math.random() * maxFloors);
                     var to = parseInt(Math.random() * maxFloors);
                     if (to === from) {
@@ -68,6 +68,6 @@ define(["underscore", "traveler", "elevator"], function(_, Traveler, Elevator) {
                     system.addTraveler(new Traveler(getName(), from, to));
                 }
             }
-        }
+        };
     };
 });

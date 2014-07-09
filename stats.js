27ceca3a0bdb1ec35adcb86f957ff7ebb91003ad
Daniel Winther <daniel.winther@blocket.se>
2014-07-09 16:22:27 +0200

    Now calculates mean, variance and stddev for traveler times

--- stats.js
+++ stats.js
@@ -1,4 +1,21 @@
 define(["underscore"], function(_) {
+    function mean(samples) {
+        return _.reduce(samples, function(memo, num) {
+            return memo + num;
+        }, 0) / samples.length;
+    }
+
+    function variance(samples) {
+        var m = mean(samples);
+        return _.reduce(samples, function(memo, num) {
+            return memo + Math.pow(num - m, 2);
+        }, 0) / samples.length;
+    }
+
+    function stddev(samples) {
+        return Math.sqrt(variance(samples));
+    }
+
     return function(logger, options) {
         var timeline = [];
         var travelers = [];
@@ -22,21 +39,23 @@ define(["underscore"], function(_) {
 
         this.onEnd = function() {
             //console.info(JSON.stringify(timeline));
-            var minWaited = _.min(_.map(travelers, function(item) {
+            var waited = _.map(travelers, function(item) {
                 return item.waited;
-            }));
-            var maxWaited = _.max(_.map(travelers, function(item) {
-                return item.waited;
-            }));
-            var minGoing = _.min(_.map(travelers, function(item) {
+            });
+            var going = _.map(travelers, function(item) {
                 return item.going;
-            }));
-            var maxGoing = _.max(_.map(travelers, function(item) {
-                return item.going;
-            }));
-            logger.log("STATS");
-            logger.log("Min wait time: " + minWaited + " Max wait time: " + maxWaited);
-            logger.log("Min going time: " + minGoing + " Max going time: " + maxGoing);
+            });
+            logger.log("");
+            logger.log("Wait time");
+            logger.log("=========");
+            logger.log("Min: " + _.min(waited) + ", max: " + _.max(waited));
+            logger.log("Mean: " + mean(waited) + ", variance: " + variance(waited) + ", stddev: " + stddev(waited));
+            logger.log("");
+            logger.log("Going time");
+            logger.log("=========");
+            logger.log("Min: " + _.min(going) + ", max: " + _.max(going));
+            logger.log("Mean: " + mean(going) + ", variance: " + variance(going) + ", stddev: " + stddev(waited));
+            logger.log("");
             logger.log("Number of ticks: " + timeline.length);
             logger.log("");
             logger.success(JSON.stringify(timeline));

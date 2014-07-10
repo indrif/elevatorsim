8083da7e553b6e27ce093867bba3163bc5d83db2
Daniel Winther <daniel.winther@blocket.se>
2014-07-10 14:36:03 +0200

    100% coverage for tests

--- test/elevator.js
+++ test/elevator.js
@@ -3,16 +3,29 @@ var expect = require("chai").expect,
     Elevator = require("../lib/elevator");
 
 describe("elevator", function () {
-	var elevator, traveler;
+	var elevator, traveler, stats;
+
+	function moveElevatorToFloor(elevator, floor) {
+		var current = elevator.getState().floor;
+		elevator.startMovingToFloor(floor);
+		for(var floors = 0; floors < Math.abs(floor - current); floors++) {
+			for(var i = 2; i > 0; i--) {
+				elevator.onTick();
+			}
+		}
+	}
+
 	beforeEach(function () {
 		traveler = {
-			getToFloor: function () {return 1; },
+			getToFloor: function () {return 2; },
 			getFromFloor: function () {return 0; },
 			getId: function () {return "id"; },
-			onEnterElevator: function () {}
+			onEnterElevator: function () {},
+			onTick: function () {}
 		};
         stats = {
-            onTravelerUnloaded: function() {}
+            onTravelerUnloaded: function() {},
+            onTick: function() {}
         };
         elevator = new Elevator("id", 1, 10, 2, 3, stats);
 	});
@@ -48,7 +61,7 @@ describe("elevator", function () {
 		elevator.addTraveler(traveler);
 		var state = elevator.getState();
 		expect(state.travelers.length).to.equal(1);
-		expect(state.travelers[0].to).to.equal(1);
+		expect(state.travelers[0].to).to.equal(2);
 		expect(state.travelers[0].from).to.equal(0);
 		expect(state.travelers[0].id).to.equal("id");
 	});
@@ -60,24 +73,20 @@ describe("elevator", function () {
 		elevator.onTick();
 	});
 	it("should move between floors when called", function () {
-		elevator.startMovingToFloor(2);
 		var state;
-		for(var i = 2; i > 0; i--) {
-			state = elevator.getState();
-			expect(state.ticksLeft).to.equal(i);
-			elevator.onTick();
-		}
+
+		// Move down
+		moveElevatorToFloor(elevator, 2);
 		state = elevator.getState();
 		expect(state.floor).to.equal(2);
+
+		// Move up
+		moveElevatorToFloor(elevator, 1);
+		state = elevator.getState();
+		expect(state.floor).to.equal(1);
 	});
 	it("should open on floor when moving finishes", function (done) {
-		elevator.startMovingToFloor(2);
-		var state;
-		for(var i = 2; i > 0; i--) {
-			state = elevator.getState();
-			expect(state.ticksLeft).to.equal(i);
-			elevator.onTick();
-		}
+		moveElevatorToFloor(elevator, 2);
 		state = elevator.getState();
 		expect(state.floor).to.equal(2);
 		elevator.onTick();
@@ -94,5 +103,16 @@ describe("elevator", function () {
 
 		state = elevator.getState();
 		expect(state.state).to.equal("still");
+	});
+	it("should keep track of travelers leaving", function (done) {
+		elevator.addTraveler(traveler);
+		stats.onTravelerUnloaded = function (item) {
+			expect(item.getToFloor()).to.equal(2);
+			done();
+		};
+		moveElevatorToFloor(elevator, 2);
+		for(var i = 0; i < 6; i++) {
+			elevator.onTick(null, function () {});
+		}
 	});
 });
